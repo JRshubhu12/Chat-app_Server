b37ca6b83cb1278c906592305b2e5d2c32b5708e
Shubham Choudhary <86393049+JRshubhu12@users.noreply.github.com>
2024-08-12 23:39:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://fabulous-empanada-9e0abf.netlify.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
